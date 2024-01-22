5214127372623b361ada2c6695ec5dd4f62e7dc5
atharvadesai1 <113234377+atharvadesai1@users.noreply.github.com>
2024-01-23 02:31:32 +0530

    doing

--- src/pages/questions/Stressq.js
+++ src/pages/questions/Stressq.js
@@ -5,47 +5,47 @@ const squestion = [
         "qno":"q1"
     },
     {
-        "que": "Q2] In the last month, how often have you felt that you were unable to control the important things in your life?",
+        "que": "Q2) In the last month, how often have you felt that you were unable to control the important things in your life?",
         "options":[["",0],["Never",0],["Almost Never",1],["Sometimes",2],["Fairly Often",3],["Very Often",4]],
         "qno":"q2"
     },
     {
-        "que": "Q3] In the last month, how often have you felt nervous and stressed?",
+        "que": "Q3) In the last month, how often have you felt nervous and stressed?",
         "options":[["",0],["Never",0],["Almost Never",1],["Sometimes",2],["Fairly Often",3],["Very Often",4]],
         "qno":"q3"
     },
     {
-        "que": "Q4] In the last month, how often have you felt confident about your ability to handle your personal problems?",
+        "que": "Q4) In the last month, how often have you felt confident about your ability to handle your personal problems?",
         "options":[["",0],["Never",4],["Almost Never",3],["Sometimes",2],["Fairly Often",1],["Very Often",0]],
         "qno":"q4"
     },
     {
-        "que": "Q5] In the last month, how often have you felt that things were going your way?",
+        "que": "Q5) In the last month, how often have you felt that things were going your way?",
         "options":[["",0],["Never",4],["Almost Never",3],["Sometimes",2],["Fairly Often",1],["Very Often",0]],
         "qno":"q5"
     },
     {
-        "que": "Q6] In the last month, how often have you found that you could not cope with all the things that you had to do?",
+        "que": "Q6) In the last month, how often have you found that you could not cope with all the things that you had to do?",
         "options":[["",0],["Never",0],["Almost Never",1],["Sometimes",2],["Fairly Often",3],["Very Often",4]],
         "qno":"q6"
     },
     {
-        "que": "Q7] In the last month, how often have you been able to control irritations in your life?",
+        "que": "Q7) In the last month, how often have you been able to control irritations in your life?",
         "options":[["",0],["Never",4],["Almost Never",3],["Sometimes",2],["Fairly Often",1],["Very Often",0]],
         "qno":"q7"
     },
     {
-        "que": "Q8] In the last month, how often have you felt that you were on top of things?",
+        "que": "Q8) In the last month, how often have you felt that you were on top of things?",
         "options":[["",0],["Never",4],["Almost Never",3],["Sometimes",2],["Fairly Often",1],["Very Often",0]],
         "qno":"q8"
     },
     {
-        "que": "Q9] In the last month, how often have you been angered because of things that happened that were outside of your control?",
+        "que": "Q9) In the last month, how often have you been angered because of things that happened that were outside of your control?",
         "options":[["",0],["Never",0],["Almost Never",1],["Sometimes",2],["Fairly Often",3],["Very Often",4]],
         "qno":"q9"
     },
     {
-        "que": "Q10] In the last month, how often have you felt difficulties were piling up so high that you could not overcome them?",
+        "que": "Q10) In the last month, how often have you felt difficulties were piling up so high that you could not overcome them?",
         "options":[["",0],["Never",0],["Almost Never",1],["Sometimes",2],["Fairly Often",3],["Very Often",4]],
         "qno":"q10"
     }
