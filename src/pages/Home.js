a8767d75aa9c20334e541a0ab370eb69aa5c684b
atharvadesai1 <113234377+atharvadesai1@users.noreply.github.com>
2024-01-26 21:52:47 +0530

    doing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import '../styles/Home.css'
-import ImageCenter from '../images/bag.png'
+import ImageCenter from '../images/bag3.png'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
 
 function Home() {
   return (
     <Layout>
         <div>
         <div className='imagecenter' style={{backgroundImage:`url(${ImageCenter})`}}>
-          {/* <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis ipsam sapiente magni ab fugiat? Eligendi sed sapiente doloribus praesentium laudantium doloremque dolorum pariatur aspernatur soluta quis, voluptates, ex necessitatibus architecto!</h4> */}
+          <div className="greenbox">       
+          </div>
+          <div className="forminfo">
+              <h1>Twitter Information</h1>
+              <form action="">
+                <Box className='formp' component="form" sx={{'& .MuiTextField-root': { ml:3,mr:3,mt:2,mb:2, width: '35ch' },}} noValidate autoComplete="off">
+                    <TextField required id="outlined-basic" label="Full Name" variant="outlined" /><br />
+                    <TextField required id="outlined-basic" label="Email ID" variant="outlined" type='email' /><br />
+                    <TextField required id="outlined-basic" label="Twitter ID" variant="outlined" /><br />
+                    <TextField required id="outlined-password-input" label="Password" type="password" autoComplete="current-password"/><br />
+                </Box><br />
+                <Button variant="contained" color="secondary" type='submit' sx={{marginBottom:'15px', borderColor:'black'}}>Submit</Button>
+              </form>
+            </div> 
         </div>
         </div>
     </Layout>
